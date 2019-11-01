6546687de9c33df2c5030d012dc778615501ec36
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-01 12:03:19 -0400

    Refactoring components to individual modules

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FirstComponent from './components/learning-examples/FirstComponent';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,11 +14,6 @@ class App extends Component {
         <FourthComponent />
       </div>
     );
-  }
-}
-class FirstComponent extends Component {
-  render() {
-    return <div className='firstComponent'>First Compenent</div>;
   }
 }
 
---
+++ todo-app/src/components/learning-examples/FirstComponent.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class FirstComponent extends Component {
+  render() {
+    return <div className='firstComponent'>First Compenent</div>;
+  }
+}
