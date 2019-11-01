b236d34a8c10bae8cd26b5b9b0846193892928cb
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-01 12:46:30 -0400

    Refactoring components to individual modules

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import FirstComponent from './components/learning-examples/FirstComponent';
+import SecondComponent from './components/learning-examples/SecondComponent';
+import ThirdComponent from './components/learning-examples/ThirdComponent';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,24 +14,9 @@ class App extends Component {
         <FirstComponent />
         <SecondComponent />
         <ThirdComponent />
-        <FourthComponent />
       </div>
     );
   }
-}
-
-class SecondComponent extends Component {
-  render() {
-    return <div className='secondComponent'>Second Compenent</div>;
-  }
-}
-
-function ThirdComponent() {
-  return <div className='thirdComponent'>Third Compenent</div>;
-}
-
-function FourthComponent() {
-  return <div className='fourthComponent'>Fourth Component</div>;
 }
 
 export default App;
