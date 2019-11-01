abe3388f4f9e320d04954396c5f4b323911a31ed
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-01 13:08:42 -0400

    Create counter component

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import FirstComponent from './components/learning-examples/FirstComponent';
 import SecondComponent from './components/learning-examples/SecondComponent';
 import ThirdComponent from './components/learning-examples/ThirdComponent';
+import Counter from './components/counter/counter';
 
 import logo from './logo.svg';
 import './App.css';
@@ -10,6 +11,16 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
+        <Counter />
+      </div>
+    );
+  }
+}
+
+class LearningComponents extends Component {
+  render() {
+    return (
+      <div className='LearningComponents'>
         My Hello world
         <FirstComponent />
         <SecondComponent />
---
+++ todo-app/src/components/counter/counter.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+function Counter() {
+  return <div className='counter'>Counter Created</div>;
+}
+
+export default Counter;
