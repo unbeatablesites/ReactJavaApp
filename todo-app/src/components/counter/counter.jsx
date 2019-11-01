ba4e74f6d06889de27f3711e689e6811d247067b
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-01 14:22:41 -0400

    Implemented constructor added super and binded function

--- todo-app/src/components/counter/counter.jsx
+++ todo-app/src/components/counter/counter.jsx
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 import './Counter.css';
 
 class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    };
+    this.increment = this.increment.bind(this);
+  }
+
   render() {
     return (
       <div className='counter'>
-        <button onClick={this.increment}>+1</button>
-        <span className='count'>0</span>
+        <button onClick={this.increment}>+1</button>{' '}
+        {/* // refers to the method */}
+        <span className='count'>{this.state.counter}</span>{' '}
+        {/* refers to the constructor "uses super first" */}
       </div>
     );
   }
   increment() {
-    console.log('increment');
+    // console.log('increment');
+    // this.state.counter++;
+    this.setState({ counter: this.state.counter + 1 });
   }
 }
 
