16ce176451f1ec12f8855a377c14b0e2d8a8db8a
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-02 11:33:25 -0400

    Small bug fix

--- todo-app/src/components/counter/counter.jsx
+++ todo-app/src/components/counter/counter.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Counter.css';
 
 class Counter extends Component {
@@ -27,11 +28,11 @@ class Counter extends Component {
     });
   }
 }
-CounterButton.defaultProps = {
+Counter.defaultProps = {
   by: 1
 };
 
-CounterButton.propTypes = {
+Counter.propTypes = {
   by: PropTypes.number
 };
 export default Counter;
