76ebd0bff4debed3016cbeb81bf4a432b6c3863c
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-03 12:05:29 -0500

    Small refactor

--- todo-app/src/components/counter/counter.jsx
+++ todo-app/src/components/counter/counter.jsx
@@ -42,9 +42,7 @@ class Counter extends Component {
   }
 
   reset() {
-    this.setState(prevState => {
-      return { counter: 0 };
-    });
+    this.setState({ counter: 0 });
   }
 
   increment(by) {
