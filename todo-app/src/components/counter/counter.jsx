6a5baad3b652cec3f86103e8d51b878a9c53fc42
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-03 12:37:53 -0500

    refactor

--- todo-app/src/components/counter/counter.jsx
+++ todo-app/src/components/counter/counter.jsx
@@ -4,10 +4,12 @@ import './Counter.css';
 
 class Counter extends Component {
   constructor() {
-    super();
+    super(); //Error 1
+
     this.state = {
       counter: 0
     };
+
     this.increment = this.increment.bind(this);
     this.decrement = this.decrement.bind(this);
     this.reset = this.reset.bind(this);
@@ -46,58 +48,70 @@ class Counter extends Component {
   }
 
   increment(by) {
-    console.log(`increment from child - ${by}`);
-    // this.state.counter++;
+    //console.log(`increment from child - ${by}`)
     this.setState(prevState => {
       return { counter: prevState.counter + by };
     });
   }
 
   decrement(by) {
-    console.log(`decrement from child - ${by}`);
-    // this.state.counter++;
+    //console.log(`increment from child - ${by}`)
     this.setState(prevState => {
       return { counter: prevState.counter - by };
     });
   }
 }
+
 class CounterButton extends Component {
-  constructor() {
-    super();
-    // this.state = {
-    //   counter: 0
-    // };
-    // this.increment = this.increment.bind(this);
-    // this.decrement = this.decrement.bind(this);
-  }
+  //Define the initial state in a constructor
+  //state => counter 0
+  //constructor() {
+  //    super(); //Error 1
+
+  //   this.state = {
+  //       counter : 0
+  //   }
+
+  //   this.increment = this.increment.bind(this);
+  //   this.decrement = this.decrement.bind(this);
+  //}
 
   render() {
-    // const style = { fontSize: '50px', padding: '15px 30px' };
+    //render = () =>  {
+    //const style = {fontSize : "50px", padding : "15px 30px"};
     return (
       <div className='counter'>
-        <button onClick={this.increment}>+{this.props.by}</button>
-        <button onClick={this.decrement}>-{this.props.by}</button>
+        <button onClick={() => this.props.incrementMethod(this.props.by)}>
+          +{this.props.by}
+        </button>
+        <button onClick={() => this.props.decrementMethod(this.props.by)}>
+          -{this.props.by}
+        </button>
+        {/*<span className="count" 
+            >{this.state.counter}</span>*/}
       </div>
     );
   }
-  // increment() {
-  //   // console.log('increment');
-  //   // this.state.counter++;
-  //   this.setState({
-  //     counter: this.state.counter + this.props.by
-  //   });
-  //   this.props.incrementMethod(this.props.by);
-  // }
 
-  // decrement() {
-  //   // console.log('increment');
-  //   // this.state.counter++;
-  //   this.setState({
-  //     counter: this.state.counter - this.props.by
-  //   });
-  //   this.props.decrementMethod(this.props.by);
-  // }
+  //   increment() { //Update state - counter++
+  //    //console.log('increment');
+  //     //this.state.counter++; //Bad Practice
+  //     this.setState({
+  //         counter: this.state.counter + this.props.by
+  //     });
+
+  //     this.props.incrementMethod(this.props.by);
+  //   }
+
+  //   decrement () {
+  //     this.setState({
+  //         counter: this.state.counter - this.props.by
+  //     });
+
+  //     this.props.decrementMethod(this.props.by);
+  //   }
 }
+
 CounterButton.defaultProps = {
   by: 1
 };
@@ -105,4 +119,5 @@ CounterButton.defaultProps = {
 CounterButton.propTypes = {
   by: PropTypes.number
 };
+
 export default Counter;
