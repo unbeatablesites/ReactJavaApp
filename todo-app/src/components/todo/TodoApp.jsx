cbf0fc01bf1376646a3120af4b2ae0b3eacb0352
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-04 20:09:30 -0500

    Create Login div

--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -5,4 +5,11 @@ class TodoApp extends Component {
     return <div className='TodoApp'>Working</div>;
   }
 }
+
+class LoginComponent extends Component {
+  render() {
+    return <div>Login component </div>;
+  }
+}
+
 export default TodoApp;
